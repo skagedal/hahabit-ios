8a3f90d936f998ba314b64da014b4addbc344e5f
Simon Kågedal Reimer <skagedal@gmail.com>
2023-04-23 16:24:30 +0200

    Initial commit

---
+++ hahabit-ios/hahabit_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  hahabit_iosApp.swift
+//  hahabit-ios
+//
+//  Created by Simon Kågedal Reimer on 2023-04-23.
+//
+
+import SwiftUI
+
+@main
+struct hahabit_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
